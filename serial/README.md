c846a578089d81c73d26808373b58de016b74325
Gabriel Minton <gminton07@gmail.com>
2026-02-28 00:30:49 -0600

    Create serial branch

---
+++ serial/README.md
@@ -0,0 +1,1 @@
+Here are backups of RP2040 files. And a script to interface from the host computer. usb_testing.py should work on all (Windows/Linux) platforms.
---
+++ serial/usb_testing.py
@@ -0,0 +1,37 @@
+# Python USB CDC script
+# PC version
+
+import serial, threading, time
+import platform
+
+# Dynamically set PORT based on the system OS
+PLATFORM = platform.system()
+if PLATFORM == 'Windows':
+    PORT = 'COM6'
+elif PLATFORM == 'Linux':
+    PORT = '/dev/ttyACM0'
+
+BAUD = 115200
+
+ser = serial.Serial(PORT, BAUD, timeout=1)
+time.sleep(2)       # Allow board to fully reset
+
+def read_from_board():
+    while True:
+        if ser.in_waiting:
+            line = ser.readline().decode(errors='ignore').strip()
+            if line:
+                print('RP2040 -> PC:', line)
+
+def write_to_board():
+    while True:
+        msg = input('PC -> ')
+        ser.write((msg + '\n').encode())
+
+
+# Start background reader
+reader = threading.Thread(target=read_from_board, daemon=True)
+reader.start()
+
+print("Connected to RP2040")
+write_to_board()
