20eb9b83140221404beaffe435a978061c83f298
danieloliva <d.j.oliva@wustl.edu>
2026-04-09 22:44:02 -0500

    Modified files

--- Development/Camera/Camera_Test.py
+++ Development/Camera/Camera_Test.py
@@ -7,8 +7,10 @@ from Take_Image import Capture_Image
 # Set up the arguments #
 parser = argparse.ArgumentParser()
 parser.add_argument('--debug',help='helps to debug by saving images to SD rather than ram and provides debug statements',action='store_true')
-parser.add_argument('-resolution',help='adjust the resolution of the camera for speed and memory sake', type = int, nargs='+', default = [1640,1232])
+parser.add_argument('--resolution',help='adjust the resolution of the camera for speed and memory sake', type = int, nargs='+', default = [1640,1232])
 parser.add_argument('--sleep', help='time delay after taking image', type = float, default = .1)
+parser.add_argument('--Gradient', help = 'Alter the gradient min and max for Canny detection'. type = int, nargs='+', default = [50,85] )
+parser.add_argument('--ApertureSize', help='Adjust the Canny algorithm to find filter the magnitude of the gradient for line detection. Gradients are computed using the convolution of a pixel, possible alternative is the scharr function, they are both discrete derivative aproximations', type = int, default=5)
 args = parser.parse_args()
 
 # Select the Camera Resolution #
@@ -28,9 +30,14 @@ gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Blur out the image interpolating a 5x5 area surrounding pixel #
 blur_gray_img = cv2.blur(gray_img,(5,5))
+
 if args.debug:
     cv2.imwrite('test_gray_img.png',blure_gray_img)
     print('saving the image as a test image')
+
+# Apply Canny detection #
+canny_img = cv2.Canny(blur_gray_img, args.Gradient[0], args.Gradient[1],apertureSize=args.ApertureSize)
+
 
 # Apply the lines detection #
 lines = cv2.HoughLinesP(blur_gray_img, 1, np.pi/180, threshold = 100, minLineLength=20, maxLineGap=10)
--- Development/Camera/Gradient.py
+++ Development/Camera/Gradient.py
@@ -1,10 +1,16 @@
 import cv2
+import numpy as np
 
 # Import the desired file for the gradient analysis #
 filename = input('Enter File Here: ')
 orig_img = cv2.imread(filename)
 
+# determine the image size #
+y_height = 1640
+x_width = 1232
+
 # Find the imae shape and convert the image format #
+orig_img = cv2.resize(orig_img, (y_height,x_width), cv2.INTER_LINEAR)
 height,width = orig_img.shape[:2]
 gray_img = cv2.cvtColor(orig_img, cv2.COLOR_BGR2GRAY)
 blur_gray_img = cv2.blur(gray_img,(5,5))
@@ -13,49 +19,57 @@ new_img = orig_img.copy()
 def blank(x):
     pass;
 
-# This code formats the window #
-verstr = (f'vert 0: {height-1}')
-horzstr =(f'hort 0: {width-1}')
-cv2.namedWindow('window',cv2.WINDOW_NORMAL)
 
+cv2.namedWindow('window',cv2.WINDOW_NORMAL)
 cv2.resizeWindow('window',width,height)
-cv2.createTrackbar(verstr,'window',0,height-2,blank)
-cv2.createTrackbar(horzstr,'window',0,width-2, blank)
+
+
+# This code formats the window #
+verstr = 'vert_1'
+verstr2 = 'vert_2'
+horzstr='horz_1'
+horzstr2 = 'horz_2'
+
+
+cv2.createTrackbar(verstr,'window',0,height-1,blank)
+cv2.createTrackbar(horzstr,'window',0,width-1, blank)
 
 # Adding another tracker bar to determine the gradient #
-cv2.createTrackbar(verstr,'window',0,height-1,blank)
-cv2.createTrackbar(horzstr,'window',0,width-1,blank)
+cv2.createTrackbar(verstr2,'window',height-1 ,height-1,blank)
+cv2.createTrackbar(horzstr2,'window', width-1 ,width-1,blank)
 
 
 while True:
+    # Create a new overlay to prevent saved circles when testing pixels#
+    img = blur_gray_img.copy()
 
     # Creates a red circle #
     lc = cv2.getTrackbarPos(verstr, 'window')
     hc = cv2.getTrackbarPos(horzstr, 'window')
-    new_img = cv2.circle(blur_gray_img, (hc,lc), radius = int(width*0.005),color=(0,0,255), thickness=int(width*.002))
 
     # Creates a blue circle to know the position #
-    lc2 = cv2.getTrackbarPos(verstr, 'window')
-    hc2 = cv2.getTrackbarPos(horzstr, 'window')
-    new_img = cv2.circle(blur_gray_img, (hc2,lc2), radius = int(width*0.005),color=(255,0,0), thickness=int(width*.002)) 
+    lc2 = cv2.getTrackbarPos(verstr2, 'window')
+    hc2 = cv2.getTrackbarPos(horzstr2, 'window')
 
 
     # obtain the value of at the located point #
-    g1val = blur_gray_img[lc,hc]     # First point value #
-    g2val = blur_gray_img[lc2,hc2]   # Second point value #
-    gval = g2val - g1val               # The gradient or the differential #
+    g1val = int(blur_gray_img[lc,hc])     # First point value #
+    g2val = int(blur_gray_img[lc2,hc2])   # Second point value #
+    gval = np.abs(int(g2val - g1val))               # The gradient or the differential #
 
-    cv2.imshow('window',new_img)
+    # Set the circular image #
+    cv2.circle(img, (hc,lc), radius = int(width*0.005),color=(255,0,0), thickness=int(width*.002)) 
+    cv2.circle(img, (hc2,lc2), radius = int(width*0.005),color=(255,0,0), thickness=int(width*.002)) 
+
     outstring = (f'G1:{g1val}\t G2: {g2val}\tGradient: {gval}')
     cv2.setWindowTitle('window',outstring)
+    cv2.imshow('window',img)
 
     if cv2.waitKey(1) == 32:
         cv2.destroyAllWindows()
         outstring = (f'({lc}, {hc}) H:{hval} S:{sval} V:{vval}')
 
         # Place the circles on the blur grayed image, not OG image #
-        orig_img = cv2.circle(blur_gray_img, (hc,lc), int(width*.005), color=(0,0,255), thickness=int(width*.002))
-        orig_img = cv2.circle(blur_gray_img, (hc2,lc2), int(width*.005), color=(255,0,0), thickness=int(width*.002))
-        orig_img = cv2.putText(blur_gray_img, outstring, (height//10,width//10), cv2.FONT_HERSHEY_SIMPLEX,2,(0,0,255),5)
-        cv2.imwrite('new_img.jpg',blur_gray_img)
+        orig_img = cv2.putText(img, outstring, (10,10), cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
+        cv2.imwrite('filtered_img.jpg',img)
         break;
