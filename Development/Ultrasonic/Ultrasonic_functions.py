7ecdcd0a5d2504f974f5573f3095e3bf1fa647ed
danieloliva <d.j.oliva@wustl.edu>
2026-04-17 16:50:43 -0500

    changes

--- Development/Ultrasonic/Ultrasonic_functions.py
+++ Development/Ultrasonic/Ultrasonic_functions.py
@@ -20,23 +20,24 @@ def Initialize_Ult(pin_read, pin_out)
 def Distance_Read(Read,Trigger) # This is for setting the pin layout #
 
     GPIO.output(Trigger,True)
-    time.sleep(.000025) # Sendout one pulse #
+    time.sleep(.00001) # Sendout one pulse #
     GPIO.output(Trigger,False)
+
     # Read right after signal left
     start_time = time.time()
 
     # Check when the read in pin detects the ultrasonic reflection #
-    while GPIO.input(Read) == True
+    while GPIO.input(Read) == False:
         end_time = time.time()
 
     # Find when the it stops detecting (This would be the true travel dT) #
-    while GPIO.input(Read) == False
+    while GPIO.input(Read) == True:
         end_time_prime = time.time()
 
 
 
     # Find the time difference #
-    delta_T = end_time - start_time
+    delta_T = end_time_prime - start_time
 
     # Speed of sound in room temperature #
     V_temp = 340  # Set to meters/s #
